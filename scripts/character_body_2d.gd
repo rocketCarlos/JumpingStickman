f154d0ada767f49818b43093187b6c2064dda37c
Carlos Muñoz Sánchez <mscarlos.contact@gmail.com>
2025-01-12 00:44:45 +0100

    sketch for front kick

--- scripts/character_body_2d.gd
+++ scripts/character_body_2d.gd
@@ -37,6 +37,7 @@ var gravity_tween: Tween
 func _ready():
 	FLOOR_LEVEL += position.y
 	JUMP_LEVEL = position.y + JUMP_LEVEL
+	#Engine.time_scale = 0.25
 
 func _physics_process(delta: float) -> void:
 	match current_state:
@@ -50,13 +51,19 @@ func _physics_process(delta: float) -> void:
 				if Input.is_action_just_pressed("jump"):
 					animation.play("jump")
 					current_state = states.ACTION
+				
 			elif not gravity_tween: # if not in the floor, simulate gravity
+				# set the sprite to the falling one
+				animation.animation = "fall"
 				gravity_tween = get_tree().create_tween().set_trans(Tween.TRANS_EXPO).set_ease(Tween.EASE_IN)
 				gravity_tween.tween_property(self, "position", Vector2(position.x, FLOOR_LEVEL), 0.5)
 				await gravity_tween.finished
 				gravity_tween = null
 			
+			# handle actions
 			if Input.is_action_just_pressed("front_kick"):
+				current_state = states.ACTION
+				animation.play("front_kick")
 				if gravity_tween:
 					gravity_tween.stop()
 					gravity_tween = null
@@ -66,6 +73,12 @@ func _physics_process(delta: float) -> void:
 				var v_tween = get_tree().create_tween().set_trans(Tween.TRANS_CIRC).set_ease(Tween.EASE_OUT)
 				v_tween.tween_property(self, "position", Vector2(position.x, JUMP_LEVEL), 0.25)
 				await v_tween.finished
+				current_state = states.DEFAULT
+			
+			elif animation.animation == "front_kick":
+				await animation.animation_finished
+				animation.play_backwards("front_kick")
+				await animation.animation_finished
 				current_state = states.DEFAULT
 				
 					
